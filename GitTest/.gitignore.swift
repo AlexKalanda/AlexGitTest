4a18e6c5d4d24a891244119f7957a2d87de5c211
Name <email@example.ru>
2023-10-03 21:32:13 +0300

    two commit

---
+++ GitTest/.gitignore.swift
@@ -0,0 +1,8 @@
+//
+//  .gitignore.swift
+//  GitTest
+//
+//  Created by admin on 3/10/2023.
+//
+
+import Foundation
---
+++ GitTest/error.swift
@@ -0,0 +1,8 @@
+//
+//  error.swift
+//  GitTest
+//
+//  Created by admin on 3/10/2023.
+//
+
+import Foundation
